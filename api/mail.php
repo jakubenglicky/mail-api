49ef42a97c9daccdd0bc6feed63cc8cd4bb933db
jakubenglicky <jakubenglicky@gmail.com>
2020-06-09 18:58:30 +0200

    Test

--- api/mail.php
+++ api/mail.php
@@ -2,6 +2,18 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-var_dump(getenv());
+$smtpMailer = new \Nette\Mail\SmtpMailer([
+	'host' => \getenv('SMTP_HOST'),
+	'username' => \getenv('SMTP_USER'),
+	'password' => \getenv('SMTP_PASSWORD'),
+	'secure' => 'ssl',
+]);
+
+$msg = new \Nette\Mail\Message();
+$msg->addTo('jakubenglicky@gmail.com');
+$msg->setSubject('Ahoj');
+$msg->setBody('ahoj');
+
+$smtpMailer->send($msg);
 
 
